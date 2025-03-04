7137bb1db51e896048bbf37629da0c83bf8b2f6e
Fairkid <info@fairkid.ca>
2025-03-04 02:06:27 -0800

    Update readme and add examples

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# go-edge-tts
+# go-edge-tts [![GoDoc](https://godoc.org/github.com/fairkid-ai/go-edge-tts?status.svg)](https://godoc.org/github.com/fairkid-ai/go-edge-tts)
 Golang implementation to use Microsoft Edge's online Text-To-Speech (TTS) service without an API key.
+
+## Installation
+```bash
+go get -u github.com/fairkid-ai/go-edge-tts
+```
+
+## Usage
+```go
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func main() {
+    option := edgetts.DefaultOption()
+	option.Voice = "en-US-AnaNeural"
+
+	c := edgetts.NewCommunication(option)
+	ch, err := c.Stream("Speech synthesis is the artificial production of human speech.")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	audioBytes := make([]byte, 0, 128)
+	for chunk := range ch {
+		if chunk.Type == edgetts.CHUNK_TYPE_AUDIO {
+			audioBytes = append(audioBytes, chunk.Data...)
+		}
+	}
+
+	if err := os.WriteFile("tts.mp3", audioBytes, 0644); err != nil {
+		log.Fatal(err)
+	}
+}
+```
+
+## License
+[MIT License](https://github.com/fairkid-ai/go-edge-tts/blob/main/LICENSE)
+
+## Credits
+This library takes inspiration from [edge-tts](https://github.com/jncraton/edge-tts) and [edge-tts-go](https://github.com/surfaceyu/edge-tts-go).
--- examples/main.go
+++ examples/main.go
@@ -1,40 +1,38 @@
-package edgetts
+package main
 
 import (
+	"log"
 	"os"
-	"testing"
+
+	edgetts "github.com/fairkid-ai/go-edge-tts"
 )
 
-func TestCommunication(t *testing.T) {
-	option := DefaultOption()
-	option.Voice = "en-US-AnaNeural"
-
-	c := NewCommunication(option)
-	ch, err := c.Stream(`(From Wikipedia)
+func main() {
+	text := `(From Wikipedia)
 	Speech synthesis is the artificial production of human speech.
 	A computer system used for this purpose is called a speech synthesizer, 
 	and can be implemented in software or hardware products. 
 	A text-to-speech (TTS) system converts normal language text into speech; 
 	other systems render symbolic linguistic representations like phonetic transcriptions into speech.
-	The reverse process is speech recognition.`)
+	The reverse process is speech recognition.`
+
+	option := edgetts.DefaultOption()
+	option.Voice = "en-US-AnaNeural"
+
+	c := edgetts.NewCommunication(option)
+	ch, err := c.Stream(text)
 	if err != nil {
-		t.Error(err)
-		return
+		log.Fatal(err)
 	}
 
 	audioBytes := make([]byte, 0, 128)
 	for chunk := range ch {
-		if chunk.Type == CHUNK_TYPE_AUDIO {
+		if chunk.Type == edgetts.CHUNK_TYPE_AUDIO {
 			audioBytes = append(audioBytes, chunk.Data...)
 		}
 	}
-	if err := os.MkdirAll(".local", 0755); err != nil {
-		t.Error(err)
-		return
-	}
 
-	if err := os.WriteFile(".local/tts.mp3", audioBytes, 0644); err != nil {
-		t.Error(err)
-		return
+	if err := os.WriteFile("tts.mp3", audioBytes, 0644); err != nil {
+		log.Fatal(err)
 	}
 }
